af038a0db40a99e5348724014db7f85efeea39c7
yumenetwork <yume.network@protonmail.com>
2020-02-08 15:40:09 +0100

    Better grid
    Signed-off-by: yumenetwork <yume.network@protonmail.com>

--- seven/main.py
+++ seven/main.py
@@ -2,27 +2,25 @@ from tkinter import *
 
 myWindow = Tk()
 
-
-def create_grid():
-    for i in range(0, 300, 20):
-        grille.create_line([(i, 0), (i, 300)])
-
-    for i in range(0, 300, 20):
-        grille.create_line([(0, i), (300, i)])
-
-
 x = 0
 y = 0
 l = 0
 c = 0
 
-result_labelx = Label(myWindow, text=f"abscisse : {x}")
-result_labely = Label(myWindow, text=f"ordonnée : {x}")
-result_ligne = Label(myWindow, text=f"ligne : {l}")
-result_colonne = Label(myWindow, text=f"colonne : {c}")
-boutonGrid = Button(myWindow, text="Tracer la grille", command=create_grid)
 
-grille = Canvas(myWindow, width=300, height=300)
+# Fonctions
+def create_grid():
+    grille.delete("all")
+    for i in range(5, 305, 20):
+        grille.create_line([(i, 5), (i, 305)])
+
+    for i in range(5, 305, 20):
+        grille.create_line([(5, i), (305, i)])
+
+    grille.create_line([(5, 5), (5, 305)])
+    grille.create_line([(305, 5), (305, 305)])
+    grille.create_line([(5, 5), (305, 5)])
+    grille.create_line([(5, 305), (305, 305)])
 
 
 def get_coord(event):
@@ -36,13 +34,28 @@ def get_coord(event):
     result_colonne.config(text=f"colonne : {column}")
     result_ligne.config(text=f"ligne : {ligne}")
 
+    create_grid()
+    grille.create_line([(x - 5, y), (x + 5, y)])
+    grille.create_line([(x, y - 5), (x, y + 5)])
 
+
+# Widgets
+result_labelx = Label(myWindow, text=f"abscisse : {x}")
+result_labely = Label(myWindow, text=f"ordonnée : {x}")
+result_ligne = Label(myWindow, text=f"ligne : {l}")
+result_colonne = Label(myWindow, text=f"colonne : {c}")
+boutonGrid = Button(myWindow, text="Tracer la grille", command=create_grid)
+
+grille = Canvas(myWindow, width=310, height=310)
+
+# Affichage des Widgets
 boutonGrid.grid(row=0, column=1)
 result_labelx.grid(row=1, column=1)
 result_labely.grid(row=2, column=1)
 result_ligne.grid(row=3, column=1)
 result_colonne.grid(row=4, column=1)
 grille.grid(row=0, column=0, rowspan=5)
-myWindow.bind("<Button 1>", get_coord)
 
+# Main
+myWindow.bind("<Button 1>", get_coord)
 myWindow.mainloop()
