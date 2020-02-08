13f0095d30652cd4ca74b39cae8a3b8208e4532a
yumenetwork <yume.network@protonmail.com>
2020-02-08 15:40:41 +0100

    Add comments
    Signed-off-by: yumenetwork <yume.network@protonmail.com>

--- eight/main.py
+++ eight/main.py
@@ -4,6 +4,7 @@ root = Tk()
 root.geometry("300x300")
 
 
+# Fonctions
 def quitter():
     root.quit()
     root.destroy()
@@ -29,15 +30,18 @@ def down(event):
     draw.coords(ball, x1, y1 + 5, x2, y2 + 5)
 
 
+# Widgets
 draw = Canvas(root)
 boutonQuitter = Button(root, text='Quitter', command=quitter)
+
+# Affichage des Widgets
 draw.pack()
 boutonQuitter.pack()
-ball = draw.create_oval(100, 100, 150, 150, fill='red')
 
+# Main
+ball = draw.create_oval(100, 100, 150, 150, fill='red')
 root.bind("<Left>", left)
 root.bind("<Right>", right)
 root.bind("<Up>", up)
 root.bind("<Down>", down)
-
 root.mainloop()
